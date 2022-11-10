dd10c65ba3f0a91dddb2dfdcb84f9e76255f3379
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-10 16:13:28 +0100

    scaffolder

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <HeaderComponent />
+    </header>
+    <main>
+        <BodyComponent />
+    </main>
+    <footer>
+        <FooterComponent />
+    </footer>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import BodyComponent from './components/BodyComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+    export default {
+        components:{
+            HeaderComponent,
+            BodyComponent,
+            FooterComponent,
+        },
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BodyComponent',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
